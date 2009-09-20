a77bcdd487ce8cc5022c68cf35da01301bebbd4f
alimka <kamila.turek@gmail.com>
2009-09-20 19:31:00 +0200

    ktualna

--- zadanie4/main.cpp
+++ zadanie4/main.cpp
@@ -11,31 +11,34 @@ struct Element {
     int poz;
 };
 
-bool cmp (Element i, Element j) { // posortuj do lewej wzg. końców przedziałów (jeśli końce są równe to początków)
+bool cmp (const Element& i, const Element& j) { // posortuj do lewej wzg. końców przedziałów (jeśli końce są równe to początków)
     if (i.kn != j.kn) return (i.kn < j.kn);
-    return (i.st > j.st);
+    return (i.st < j.st);
 }
 
-vector<Element> vec; // vector elementów
+Element vec[10001]; // vector elementów
 int wynik[10001]; // tablica wyników
 int flaga[10001];
 
 int main()
 {
     int n;
-    Element a;
+    // Element a;
     int i, j;
     int licznik_wiez = 0;
 
     scanf("%d", &n);
     for (i = 0; i < n; ++i)
     {
-        scanf("%d %d", &a.st, &a.kn);
-        a.poz = i;
-        vec.push_back(a);
+        scanf("%d %d", &vec[i].st, &vec[i].kn);
+        vec[i].poz = i;
+        // vec[i] = a;
+        // vec.push_back(a);
     }
 
-    sort(vec.begin(), vec.end(), cmp);
+    // sort(vec.begin(), vec.end(), cmp);
+    sort(vec, vec + n, cmp);
+
 
     for (i = 0; i < n; ++i) { // dla każdego wiersza
         for (j = vec[i].st; j <= vec[i].kn; ++j) { // w obrębie dostępnego obszaru
@@ -49,4 +52,6 @@ int main()
     }
     if (licznik_wiez == n) for (int i = 0; i < n; i++) cout << wynik[i] << endl;
     else cout << "NIE" << endl;
+
+    return 0;
 }
